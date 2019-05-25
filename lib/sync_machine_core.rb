1a268ee6b364d5eb65d5e6cba339a4adec6de891
Francis Hwang <sera@fhwang.net>
2019-05-25 13:54:59 -0400

    Initial commit.

---
+++ lib/sync_machine_core.rb
@@ -0,0 +1,6 @@
+require "sync_machine_core/version"
+
+module SyncMachineCore
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sync_machine_core/version.rb
@@ -0,0 +1,3 @@
+module SyncMachineCore
+  VERSION = "0.1.0"
+end
